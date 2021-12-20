d080e9d960d752e7ee229640a494df72ae695ba1
Mesnyankin Vladislav <goosts@mail.ru>
2021-12-20 17:11:53 +0300

    Решение задачи 20 спичек. Улучшил решение: *Добавив вывод правил перед игрой *Выход из игры в любой момент *Обработка неверного ввода

---
+++ src/main/java/src/main/java/com/epam/izh/rd/online/gameService/Matches.java
@@ -0,0 +1,23 @@
+package src.main.java.com.epam.izh.rd.online.gameService;
+
+/**
+ * Класс хранит количество спичек на столе
+ * */
+public class Matches {
+    private int numberOfMatches = 20;
+
+    public int getNumberOfMatches() {
+        return numberOfMatches;
+    }
+
+    public void setNumberOfMatches(int numberOfMatches) {
+            this.numberOfMatches -= numberOfMatches;
+    }
+
+    /**
+     * вывод оставшихся спичек на столе
+     * */
+    public void printNumberOfMatches() {
+        System.out.println("На столе осталось " + numberOfMatches + " спичек.");
+    }
+}
---
+++ src/main/java/src/main/java/com/epam/izh/rd/online/gameService/UserPlayer.java
@@ -0,0 +1,73 @@
+package src.main.java.com.epam.izh.rd.online.gameService;
+
+import java.util.Scanner;
+
+/**
+ * Класс отвечает за игру "Пользователя"
+ * */
+public class UserPlayer implements Player {
+    private int userCountMatch = 1;
+    private Scanner scanner;
+    private final Matches matches;
+
+    public UserPlayer(Matches matches) {
+        this.matches = matches;
+    }
+
+    /**
+     * вывод количество выбранных спичек пользователем и уменьшает общее количество спичек
+     * */
+    @Override
+    public void makeMove() {
+        boolean isUnCorrectInput = true;
+
+        while (isUnCorrectInput) {
+            try {
+                matches.printNumberOfMatches();
+                System.out.print("- Ход игрока. Введите количество спичек: ");
+                checkInpValue(scanner.nextLine());
+                isUnCorrectInput = false;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Некорректно введено количество спичек, пожалуйста выберите значение: 1, 2 или 3 спички.");
+            }
+        }
+    }
+
+    /**
+     * проверка введенного значения на корректность, согласно правилам игры, и на завершение игры
+     *
+     * @param inpValue строка переданная пользователем
+     * */
+    private void checkInpValue(String inpValue){
+        if (inpValue.equalsIgnoreCase("exit")){
+            System.exit(0);
+        }
+        userCountMatch = Integer.parseInt(inpValue);
+        if (userCountMatch < 4 && userCountMatch > 0) {
+            matches.setNumberOfMatches(userCountMatch);
+            checkWinner();
+        } else {
+            throw new NumberFormatException();
+        }
+    }
+
+    /**
+     * проверка оставшегося количества спичек и вывод победителя
+     * */
+    private void checkWinner(){
+        if (matches.getNumberOfMatches() == 1){
+            System.out.println("Для компьютера осталась последняя спичка. Компьютер проиграл!");
+            System.exit(0);
+        }
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int getUserCountMatch() {
+        return userCountMatch;
+    }
+
+}
